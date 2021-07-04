f619421ed9e64eebcfbf703e396a918e11b4d046
Joseph <joe@uhhuhohyeah.com>
2021-07-04 16:37:40 -0700

    added comments / fixed issue with delete

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -16,17 +16,17 @@ if (window.location.pathname === '/notes') {
   noteList = document.querySelectorAll('.list-container .list-group');
 }
 
-// Show an element
+// SHOW ELEMENT
 const show = (elem) => {
   elem.style.display = 'inline';
 };
 
-// Hide an element
+// HIDE ELEMENT
 const hide = (elem) => {
   elem.style.display = 'none';
 };
 
-// activeNote is used to keep track of the note in the textarea
+// ACTIVENOTE TRACKS ACTIVE NOTE 
 let activeNote = {};
 
 const getNotes = () =>
@@ -79,9 +79,11 @@ const handleNoteSave = () => {
   });
 };
 
-// Delete the clicked note
+// DELETE NOTE
 const handleNoteDelete = (e) => {
-  // prevents the click listener for the list from being called when the button inside of it is clicked
+
+  // PREVENTS CLICK LISTENER FROM BEING CALLED
+
   e.stopPropagation();
 
   const note = e.target;
@@ -97,14 +99,15 @@ const handleNoteDelete = (e) => {
   });
 };
 
-// Sets the activeNote and displays it
+// SETS ACTIVENOTE AND DISPLAYS 
 const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
   renderActiveNote();
 };
 
-// Sets the activeNote to and empty object and allows the user to enter a new note
+// SETS ACTIVENOTE AS EMPTY AND ALLOWS FOR NEW NOTE ENTRY
+
 const handleNewNoteView = (e) => {
   activeNote = {};
   renderActiveNote();
@@ -118,7 +121,7 @@ const handleRenderSaveBtn = () => {
   }
 };
 
-// Render the list of note titles
+// LISTS NOTE TITLES
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
@@ -127,7 +130,7 @@ const renderNoteList = async (notes) => {
 
   let noteListItems = [];
 
-  // Returns HTML element with or without a delete button
+  // RETURNS HTML ELEMENT
   const createLi = (text, delBtn = true) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item');
@@ -171,7 +174,7 @@ const renderNoteList = async (notes) => {
   }
 };
 
-// Gets notes from the db and renders them to the sidebar
+// PULLS NOTES AND DISPLAYS AT LEFT PORTION OF WINDOW
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
 if (window.location.pathname === '/notes') {
